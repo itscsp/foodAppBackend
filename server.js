23b2c9bc8b61fdd672ea9d73f8638384bb31cdba
itscsp <chethanspoojary1@gmail.com>
2024-05-26 21:00:15 +0530

    Building Authenctication

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import cors from "cors"
 
 import { connectDB } from "./config/db.js";
 import foodRouter from "./routes/foodRoute.js";
+import userRouter from "./routes/userRoute.js";
 
 // App config
 const app = express();
@@ -18,6 +19,7 @@ connectDB();
 //API endpoints
 app.use("/api/food", foodRouter)
 app.use("/images", express.static('uploads')); // To get image by url
+app.use("/api/user", userRouter)
 
 app.get("/", (req, res)=>{
     res.send("API Working");
