42e6e9f022413370f78014708006738955619c91
itscsp <chethanspoojary1@gmail.com>
2024-06-12 20:30:40 +0530

    Building order controller

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -9,82 +9,93 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 // placing user order from frontned
 const placeOrder = async (req, res) => {
     const frontned_url = 'http://localhost:5173';
-    try{
+    try {
         const newOrder = new orderModel({
-            userId:req.body.userId,
-            items:req.body.items,
+            userId: req.body.userId,
+            items: req.body.items,
             amount: req.body.amount,
             address: req.body.address
         })
         await newOrder.save();
-        await userModel.findByIdAndUpdate(req.body.userId, {cartData:{}});
-        
+        await userModel.findByIdAndUpdate(req.body.userId, { cartData: {} });
+
 
         const line_items = req.body.items.map((item) => ({
-            price_data:{
-                currency:'usd',
-                product_data:{
-                    name:item.name
+            price_data: {
+                currency: 'usd',
+                product_data: {
+                    name: item.name
                 },
                 unit_amount: item.price * 100
             },
 
-            quantity:item.quantity
+            quantity: item.quantity
         }))
 
         line_items.push({
-            price_data:{
-                currency:'usd',
-                product_data:{
-                    name:"Delivery Charges"
+            price_data: {
+                currency: 'usd',
+                product_data: {
+                    name: "Delivery Charges"
                 },
-                unit_amount: 2*100
+                unit_amount: 2 * 100
             },
-            quantity:1
+            quantity: 1
         })
 
         const session = await stripe.checkout.sessions.create({
-            line_items:line_items,
-            mode:'payment',
-            success_url:`${frontned_url}/verify?success=true&orderId=${newOrder._id}`,
-            cancel_url:`${frontned_url}/verify?success=false&orderId=${newOrder._id}`,
+            line_items: line_items,
+            mode: 'payment',
+            success_url: `${frontned_url}/verify?success=true&orderId=${newOrder._id}`,
+            cancel_url: `${frontned_url}/verify?success=false&orderId=${newOrder._id}`,
 
         })
 
-        res.json({success:true, session_url:session.url})
+        res.json({ success: true, session_url: session.url })
 
-    }catch(error){
+    } catch (error) {
         console.log(error)
-        res.json({success:false, message: "Error"})
+        res.json({ success: false, message: "Error" })
     }
 }
 
 const verifyOrder = async (req, res) => {
-    const {orderId, success} = req.body;
+    const { orderId, success } = req.body;
 
     try {
-        if(success == 'true'){
-            await orderModel.findByIdAndUpdate(orderId, {payment:true});
-            res.json({success:true, message: "Paid"})
+        if (success == 'true') {
+            await orderModel.findByIdAndUpdate(orderId, { payment: true });
+            res.json({ success: true, message: "Paid" })
         } else {
             await orderModel.findByIdAndUpdate(orderId);
-            res.json({success:false, message: "Not Paid"})
+            res.json({ success: false, message: "Not Paid" })
         }
     } catch (error) {
         console.log(error);
-        res.json({success:false, message: "Error!"})
+        res.json({ success: false, message: "Error!" })
     }
 }
 
 const userOrders = async (req, res) => {
 
-    try{
-        const orders = await orderModel.find({userId:req.body.userId});
-        res.json({success:true, data:orders})
-    }catch(error) {
+    try {
+        const orders = await orderModel.find({ userId: req.body.userId });
+        res.json({ success: true, data: orders })
+    } catch (error) {
         console.log(error);
-        res.json({success:false, message:"Error"})
+        res.json({ success: false, message: "Error" })
     }
 }
 
-export {placeOrder, verifyOrder, userOrders}
+const listOrders = async (req, res) => {
+    try {
+        const orders = await orderModel.find({});
+        res.json({ success: true, data: orders })
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: 'Error' });
+
+    }
+}
+
+export { placeOrder, verifyOrder, userOrders, listOrders }
--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import authMddleware from '../middleware/auth.js'
-import { placeOrder, userOrders, verifyOrder } from '../controllers/orderController.js';
+import { listOrders, placeOrder, userOrders, verifyOrder } from '../controllers/orderController.js';
 
 const orderRouter = express.Router();
 
 orderRouter.post('/place', authMddleware, placeOrder);
 orderRouter.post('/verify', verifyOrder)
 orderRouter.post('/userorders',authMddleware, userOrders)
+orderRouter.get('/list', listOrders)
+
 export default orderRouter;
